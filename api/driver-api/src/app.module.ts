3088624cbf0fa452189c31fecdc7b3f4c31976b6
nathan <nathan.poncet.pro@gmail.com>
2023-07-13 10:54:31 +0200

    :zap: update migration

--- api/driver-api/src/app.module.ts
+++ api/driver-api/src/app.module.ts
@@ -1,19 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ClipperController } from './controller/clipper.controller';
 import { ClipperService } from './service/clipper.service';
-import {GrpcReflectionModule} from 'nestjs-grpc-reflection';
-import {grpcConfig} from './config/grpc.config';
-import {ConfigModule} from '@nestjs/config';
-import {PrismaService} from './service/prisma.service';
-import {AuthService} from './service/auth.service';
-import {CollectionService} from "./service/collection.service";
-import {CollectionController} from "./controller/collection.controller";
+import { GrpcReflectionModule } from 'nestjs-grpc-reflection';
+import { grpcConfig } from './config/grpc.config';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './service/prisma.service';
+import { AuthService } from './service/auth.service';
+import { CollectionService } from './service/collection.service';
+import { CollectionController } from './controller/collection.controller';
 
 @Module({
-  imports: [
-    GrpcReflectionModule.register(grpcConfig),
-    ConfigModule.forRoot(),
-  ],
+  imports: [GrpcReflectionModule.register(grpcConfig), ConfigModule.forRoot()],
   controllers: [ClipperController, CollectionController],
   providers: [ClipperService, CollectionService, PrismaService, AuthService],
 })
